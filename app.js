286deb47635af624e3b4d526419360896f67cc5b
TonWitsarut <tonokhot@gmail.com>
2018-01-29 20:34:57 +0700

    First

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var cors = require('cors')
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors())
 app.use('/', index);
 app.use('/users', users);
 
--- routes/index.js
+++ routes/index.js
@@ -8,31 +8,31 @@ router.use(bodyParser.urlencoded({
 }));
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send( 'Express' );
 });
 router.post('/choice', function(req, res) {
-  let rsp = req.body.gender
-
+  let rsp = req.body.body
+  console.log(rsp)
   let ran = Math.floor((Math.random() * 3));
-  console.log(ran)
+  // console.log(ran)
   if (rsp == 0 &&ran==0) {
-    res.render('users', {result: 'เสมอ',ues: 'rock.JPG',sum : 'rock.JPG'})
+    res.send({result: 'เสมอ',ues: 'rock.JPG',sum : 'rock.JPG'})
   }else if (rsp == 0&&ran==1) {
-    res.render('users', {result: 'ชนะ',ues: 'rock.JPG',sum : 'sissor.JPG'})
+    res.send( {result: 'ชนะ',ues: 'rock.JPG',sum : 'sissor.JPG'})
   }else if (rsp == 0&&ran==2) {
-    res.render('users', {result: 'แพ้',ues: 'rock.JPG',sum : 'paper.JPG'})
+    res.send( {result: 'แพ้',ues: 'rock.JPG',sum : 'paper.JPG'})
   }else if (rsp == 1&&ran==0) {
-    res.render('users', {result: 'แพ้',ues: 'sissor.JPG',sum : 'rock.JPG'})
+    res.send( {result: 'แพ้',ues: 'sissor.JPG',sum : 'rock.JPG'})
   }else if (rsp == 1&&ran==1) {
-    res.render('users', {result: 'เสมอ',ues: 'sissor.JPG',sum : 'sissor.JPG'})
+    res.send( {result: 'เสมอ',ues: 'sissor.JPG',sum : 'sissor.JPG'})
   }else if (rsp == 1&&ran==2) {
-    res.render('users', {result: 'ชนะ',ues: 'sissor.JPG',sum : 'paper.JPG'})
+    res.send( {result: 'ชนะ',ues: 'sissor.JPG',sum : 'paper.JPG'})
   }else if (rsp == 2&&ran==0) {
-    res.render('users', {result: 'ชนะ',ues: 'paper.JPG',sum : 'rock.JPG'})
+    res.send( {result: 'ชนะ',ues: 'paper.JPG',sum : 'rock.JPG'})
   }else if (rsp == 2&&ran==1) {
-    res.render('users', {result: 'แพ้',ues: 'paper.JPG',sum : 'sissor.JPG'})
+    res.send( {result: 'แพ้',ues: 'paper.JPG',sum : 'sissor.JPG'})
   }else if (rsp == 2&&ran==2) {
-    res.render('users', {result: 'เสมอ',ues: 'paper.JPG',sum : 'paper.JPG'})
+    res.send( {result: 'เสมอ',ues: 'paper.JPG',sum : 'paper.JPG'})
   }
 });
 
